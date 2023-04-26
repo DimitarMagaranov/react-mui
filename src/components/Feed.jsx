241654b599245f98250f7dbd87e22b75d907b71d
Dimitar Magaranov <dimitar.magaranov1@gmail.com>
2023-04-26 18:06:49 +0300

    created navbar, sidebar and feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,48 @@
-import React from "react";
+import { Favorite, MoreVert, Share } from '@mui/icons-material';
+import { Avatar, Box, Card, CardActions, CardContent, CardHeader, CardMedia, IconButton, Typography } from '@mui/material';
+import React from 'react';
 
 const Feed = () => {
     return (
-        <div>Feed</div>
-    )
-}
+        <Box flex={4} p={2}>
+            <Card>
+                <CardHeader
+                    avatar={
+                        <Avatar sx={{ bgcolor: 'red' }} aria-label="recipe">
+                            R
+                        </Avatar>
+                    }
+                    action={
+                        <IconButton aria-label="settings">
+                            <MoreVert />
+                        </IconButton>
+                    }
+                    title="Shrimp and Chorizo Paella"
+                    subheader="September 14, 2016"
+                />
+                <CardMedia
+                    component="img"
+                    height="194"
+                    image="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                    alt="Paella dish"
+                />
+                <CardContent>
+                    <Typography variant="body2" color="text.secondary">
+                        This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas
+                        along with the mussels, if you like.
+                    </Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                    <IconButton aria-label="add to favorites">
+                        <Favorite />
+                    </IconButton>
+                    <IconButton aria-label="share">
+                        <Share />
+                    </IconButton>
+                </CardActions>
+            </Card>
+        </Box>
+    );
+};
 
 export default Feed;
