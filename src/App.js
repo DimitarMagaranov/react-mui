dc78d8c05530a5ba8797ffdad847cbbb317e86ad
Dimitar Magaranov <dimitar.magaranov1@gmail.com>
2023-04-26 20:16:38 +0300

    created Rightbar, dark mode completed Sidebar and Feed

--- src/App.js
+++ src/App.js
@@ -2,18 +2,35 @@ import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import Feed from './components/Feed';
 import Rightbar from './components/Rightbar';
-import { Box, Stack } from '@mui/material';
+import { Box, Stack, ThemeProvider, createTheme } from '@mui/material';
+import Add from './components/Add';
+import { useState } from 'react';
 
 function App() {
+    const [mode, setMode] = useState('light');
+
+    const darkTheme = createTheme({
+        palette: {
+            mode: mode,
+        },
+    });
+
+    const onChangeThemeHandler = () => {
+        setMode((oldTheme) => (oldTheme === 'light' ? 'dark' : 'light'));
+    };
+
     return (
-        <Box>
-            <Navbar></Navbar>
-            <Stack direction="row" spacing={2} justifyContent="space-between">
-                <Sidebar />
-                <Feed />
-                <Rightbar />
-            </Stack>
-        </Box>
+        <ThemeProvider theme={darkTheme}>
+            <Box bgcolor={'background.default'} color={'text.primary'}>
+                <Navbar></Navbar>
+                <Stack direction="row" spacing={2} justifyContent="space-between">
+                    <Sidebar onChangeThemeHandler={onChangeThemeHandler} />
+                    <Feed />
+                    <Rightbar />
+                </Stack>
+                <Add />
+            </Box>
+        </ThemeProvider>
     );
 }
 
